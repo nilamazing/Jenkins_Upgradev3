d8ac72204c9fbc640de6190b5e85200b3b9a1c14
nilamazing <duttnilanjan@gmail.com>
2021-03-23 22:48:11 +0000

    Test Polling Message

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+  @Test
+  public void greetShouldIncludePollingTestMessage() {
+    String someone = "World, Testing Jenkins Polling";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
