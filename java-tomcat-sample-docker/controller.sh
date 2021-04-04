5a940c17f2f50fc8c7043a9ec388818eac1665b3
nilamazing <duttnilanjan@gmail.com>
2021-04-04 12:39:50 +0100

    Added shell script

---
+++ java-tomcat-sample-docker/controller.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+cntnrOutPut=$(docker inspect tomcatsamplecntnr | grep Id | cut -d ':' -f 2 | cut -d ',' -f 1)
+
+echo "Listing Container ID  & Length string"
+echo $cntnrOutPut
+
+if [[ ${#cntnrOutPut} -gt 0 ]]
+then
+ echo "Container Id: $cntnrOutPut exists"
+ echo "Removing Container"
+ docker rm -f tomcatsamplecntnr
+ echo "Container removed"
+else
+ echo "Container does not exist..Creating Container"
+fi
+ docker run -p 7070:8080 -d --name tomcatsamplecntnr tomcatsamplewebapp:46
+ echo "Container Created"
