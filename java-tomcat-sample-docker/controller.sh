1c4f367be17b839a4e109ee14f4ae4192cc59c6c
nilamazing <duttnilanjan@gmail.com>
2021-04-04 17:29:54 +0100

    Edited Jenkinsfile and shell script

--- java-tomcat-sample-docker/controller.sh
+++ java-tomcat-sample-docker/controller.sh
@@ -17,6 +17,7 @@ fi
  echo "Removing target docker images"
  docker rmi $(docker images tomcatsamplewebapp -q)
  echo "Target Docker Images removed"
+ echo "Listing Contents in Curr Directory"
  echo "Building Docker image"
  docker build -t tomcatsamplewebapp:$1 .
  docker run -p 7070:8080 -d --name tomcatsamplecntnr tomcatsamplewebapp:$1
