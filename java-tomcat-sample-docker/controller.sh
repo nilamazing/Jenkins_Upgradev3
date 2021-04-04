d2c6f6dd7c4c7d26e854962789719727f89c66de
nilamazing <duttnilanjan@gmail.com>
2021-04-04 13:18:35 +0100

    Edited Jenkinsfile and shell script

--- java-tomcat-sample-docker/controller.sh
+++ java-tomcat-sample-docker/controller.sh
@@ -14,5 +14,5 @@ then
 else
  echo "Container does not exist..Creating Container"
 fi
- docker run -p 7070:8080 -d --name tomcatsamplecntnr tomcatsamplewebapp:49
+ docker run -p 7070:8080 -d --name tomcatsamplecntnr tomcatsamplewebapp:$1
  echo "Container Created"
